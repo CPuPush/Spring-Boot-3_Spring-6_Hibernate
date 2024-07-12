d92895b9936fbe42033d989028d95b27a57af0ed
CPuPush <fpakpahan44@gmail.com>
2024-07-12 12:09:23 +0700

    spring data rest

--- 04-java-spring-real-project/src/main/java/com/continental/application/dao/EmployeeRepository.java
+++ 04-java-spring-real-project/src/main/java/com/continental/application/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.continental.application.dao;
 
 import com.continental.application.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository <Employee, Integer>{
     // no need to write any code.. wow
     // and we can use EmployeeRepository with
@@ -29,5 +31,28 @@ https://docs.spring.io/spring-data/jpa/docs/current/api/org/springframework/data
 only need 3 items
 1. Entity
 2. JpaRepository: ex EmployeeRepository extends JpaRepository
-3. Maven POM dependency for:  Spring-boot-starter-data-rest
+3. Maven POM dependency for:  Spring-boot-starter-data-rest => see in application.properties
+
+! we don't need Rest and service anymore, to see these folder, checkout the branch to 04-spring-boot-rest-crud-apis/03-crud-with-spring-data-jpa
+
+* wow thats it, we don't need to code haha
+
+! data rest
+// not all the singular can handle by jpa
+? so we have to configure it manually
+@RepositoryRestResource(path="member")
+ex mouse in plural is mice, not mouses
+
+we can configure spring data rest in application.properties
+* spring.data.rest.base-path
+base path used to expose repository resources
+* spring.data.rest.default-page-size
+default size of pages for pagination
+http://localhost:8080/magic-api/members?pages=2
+ex we have 10 data, and, we assign default page size to 5, so the totalPages is 2
+
+to query again
+http://localhost:8080/magic-api/members?sort=id,desc
+* spring.data.rest.max-page-size
+maximum size of pages
 * */
